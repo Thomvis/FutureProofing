4b7b4804323b6ed9be054fadae24bb03db3a9b66
Thomas Visser <thomas.visser@gmail.com>
2015-01-18 21:45:09 +0100

    project set-up

---
+++ FutureProofing/FutureProofing.h
@@ -0,0 +1,19 @@
+//
+//  FutureProofing.h
+//  FutureProofing
+//
+//  Created by Thomas Visser on 15/01/15.
+//  Copyright (c) 2015 Thomas Visser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FutureProofing.
+FOUNDATION_EXPORT double FutureProofingVersionNumber;
+
+//! Project version string for FutureProofing.
+FOUNDATION_EXPORT const unsigned char FutureProofingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FutureProofing/PublicHeader.h>
+
+
